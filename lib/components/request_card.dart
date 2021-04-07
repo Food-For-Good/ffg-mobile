fe45d9303453a0bd89c2247f1b75d274b06d6544
Dhruv <pateldhruv0248@gmail.com>
2021-04-07 23:02:02 +0530

    request card can be expanded to get user details

--- lib/components/request_card.dart
+++ lib/components/request_card.dart
@@ -3,7 +3,7 @@ import 'package:FoodForGood/models/listing_model.dart';
 import 'package:flutter/material.dart';
 import '../constants.dart';
 
-class RequestCard extends StatelessWidget {
+class RequestCard extends StatefulWidget {
   final String title;
   final Function onAccept;
   final Function onDecline;
@@ -12,50 +12,134 @@ class RequestCard extends StatelessWidget {
   RequestCard({this.title, this.onAccept, this.onDecline, this.requestState});
 
   @override
+  _RequestCardState createState() => _RequestCardState();
+}
+
+class _RequestCardState extends State<RequestCard> {
+  bool isExpanded = false;
+  @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Container(
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          isExpanded = !isExpanded;
+        });
+      },
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Container(
           alignment: Alignment.center,
-          height: 60.0,
+          // height: 60.0,
           width: MediaQuery.of(context).size.width * 0.90,
           padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 8.0),
           decoration: BoxDecoration(
               borderRadius: BorderRadius.all(Radius.circular(8.0)),
               border: Border.all(color: kSecondaryColor, width: 2.0)),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          child: Column(
             children: [
-              Text(
-                title,
-                style: kTextStyle,
-                softWrap: true,
-              ),
               Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  SizedBox(width: 10.0),
-                  if (requestState == requestStatePending)
-                    CustomIconButton(
-                      icon: Icons.clear_rounded,
-                      onPressed: this.onDecline,
+                  Container(
+                    // color: Colors.redAccent,
+                    width: 180.0,
+                    child: FittedBox(
+                      child: Text(
+                        widget.title,
+                        style: kTextStyle,
+                        softWrap: true,
+                      ),
                     ),
-                  if (requestState != requestStateAccepted)
-                    SizedBox(
-                      width: 10.0,
-                    ),
-                  if (requestState != requestStateCompleted)
-                    CustomIconButton(
-                      icon: Icons.check_rounded,
-                      onPressed: this.onAccept,
-                    ),
-                  if (requestState == requestStateCompleted)
-                    Text('Food handed over',
-                        style: kTextStyle.copyWith(
-                            color: kPrimaryColor, fontSize: 12.0))
+                  ),
+                  Row(
+                    children: [
+                      SizedBox(width: 10.0),
+                      if (widget.requestState == requestStatePending)
+                        CustomIconButton(
+                          icon: Icons.clear_rounded,
+                          onPressed: this.widget.onDecline,
+                        ),
+                      // if (requestState != requestStateAccepted)
+                      //   SizedBox(
+                      //     width: 10.0,
+                      //   ),
+                      if (widget.requestState != requestStateCompleted)
+                        CustomIconButton(
+                          icon: Icons.check_rounded,
+                          onPressed: this.widget.onAccept,
+                        ),
+                      if (widget.requestState == requestStateCompleted)
+                        Text('Food handed over',
+                            style: kTextStyle.copyWith(
+                                color: kPrimaryColor, fontSize: 12.0))
+                    ],
+                  ),
                 ],
               ),
+              if (isExpanded)
+                Container(
+                  alignment: Alignment.topLeft,
+                  padding: EdgeInsets.all(8.0),
+                  margin: EdgeInsets.all(8.0),
+                  width: MediaQuery.of(context).size.width * 0.80,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Row(
+                              children: [
+                                Text(
+                                  'Email: ',
+                                  style: kTextStyle.copyWith(
+                                    fontSize: 14.0,
+                                    color: kSecondaryColor.withAlpha(950),
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                Text(
+                                  'abcdef',
+                                  style: kTextStyle.copyWith(
+                                    fontSize: 14.0,
+                                    color: kSecondaryColor.withAlpha(950),
+                                  ),
+                                ),
+                              ],
+                            ),
+                            SizedBox(height: 10.0),
+                            Row(
+                              children: [
+                                Text(
+                                  'Rating: ',
+                                  style: kTextStyle.copyWith(
+                                    fontSize: 14.0,
+                                    color: kSecondaryColor.withAlpha(950),
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                Text(
+                                  'abcdef',
+                                  style: kTextStyle.copyWith(
+                                    fontSize: 14.0,
+                                    color: kSecondaryColor.withAlpha(950),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ]),
+                      CustomIconButton(
+                        icon: Icons.chat,
+                        size: 30.0,
+                      )
+                    ],
+                  ),
+                )
             ],
-          )),
+          ),
+        ),
+      ),
     );
   }
 }
